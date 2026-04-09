30f197090545f97234d331cb92b5ddfaa44724e8
Hossam Hosny <HossamHosny415@gmail.com>
2026-04-09 12:12:30 +0200

    Creating Project architecture + Initializing Models

---
+++ Restaurants.Domain/Entities/Dishes.cs
@@ -0,0 +1,9 @@
+﻿namespace Restaurants.Domain.Entities;
+
+public class Dishes
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Description { get; set; }  = default!;
+    public decimal Price { get; set; }
+}
